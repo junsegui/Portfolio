55e8892ad1427e0ed1db35940a94f97e520a0c68
Juansegui <juansegui06@gmail.com>
2022-06-14 22:24:53 -0300

    commit

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 
 const EN = document.getElementById("EN");
-const ES = document.getElementById("ES")
+const ES = document.getElementById("ES");
+const primerParte = document.getElementById("primerParte")
+const toPresentation = document.getElementById("toPresentation");
+const body = document.querySelector("body")
 
 EN.addEventListener("click",(()=>{
     let url = "./en.html"
     location.href = `${url}`
 
 }))
+
+body.addEventListener("click",(e)=>{
+    console.log(e);
+})
