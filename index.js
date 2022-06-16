3c3b0918f14cc02b8c272d7686ba351d70f4259c
Juansegui <juansegui06@gmail.com>
2022-06-16 19:32:48 -0300

    commit

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const EN = document.getElementById("EN");
 const ES = document.getElementById("ES");
 const primerParte = document.getElementById("primerParte")
 const toPresentation = document.getElementById("toPresentation");
-const body = document.querySelector("body")
+const body = document.querySelector("body");
+const upButton = document.querySelector(".upButton")
 
 EN.addEventListener("click",(()=>{
     let url = "./en.html"
@@ -13,4 +14,15 @@ EN.addEventListener("click",(()=>{
 
 body.addEventListener("click",(e)=>{
     console.log(e);
+})
+
+
+
+
+window.addEventListener("scroll",()=>{
+    if(window.pageYOffset > 100){
+        upButton.classList.add("active")
+    }else{
+        upButton.classList.remove("active")
+    }
 })
--- indexEN.js
+++ indexEN.js
@@ -1,7 +1,19 @@
 const ES = document.getElementById("ES");
+const upButton = document.querySelector(".upButton")
 
 ES.addEventListener("click",(()=>{
     let url = "./index.html"
     location.href = `${url}`
 
 }))
+
+
+
+
+window.addEventListener("scroll",()=>{
+    if(window.pageYOffset > 100){
+        upButton.classList.add("active")
+    }else{
+        upButton.classList.remove("active")
+    }
+})
