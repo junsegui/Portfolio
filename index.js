9e5504acb69bec4030b7df67a4bc3aaabbd67add
Juansegui <juansegui06@gmail.com>
2022-07-03 13:04:01 -0300

    c

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-
 const EN = document.getElementById("EN");
 const ES = document.getElementById("ES");
-const primerParte = document.getElementById("primerParte")
+const primerParte = document.getElementById("primerParte");
 const toPresentation = document.getElementById("toPresentation");
 const body = document.querySelector("body");
-const upButton = document.querySelector(".upButton")
+const upButton = document.querySelector(".upButton");
 
-EN.addEventListener("click",(()=>{
-    let url = "./en.html"
-    location.href = `${url}`
+EN.addEventListener("click", () => {
+  let url = "./en.html";
+  location.href = `${url}`;
+});
 
-}))
+body.addEventListener("click", (e) => {
+  console.log(e);
+});
 
-body.addEventListener("click",(e)=>{
-    console.log(e);
-})
-
-
-
-
-window.addEventListener("scroll",()=>{
-    if(window.pageYOffset > 100){
-        upButton.classList.add("active")
-    }else{
-        upButton.classList.remove("active")
-    }
-})
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    upButton.classList.add("active");
+  } else {
+    upButton.classList.remove("active");
+  }
+});
--- indexEN.js
+++ indexEN.js
@@ -1,19 +1,15 @@
 const ES = document.getElementById("ES");
-const upButton = document.querySelector(".upButton")
+const upButton = document.querySelector(".upButton");
 
-ES.addEventListener("click",(()=>{
-    let url = "./index.html"
-    location.href = `${url}`
+ES.addEventListener("click", () => {
+  let url = "./index.html";
+  location.href = `${url}`;
+});
 
-}))
-
-
-
-
-window.addEventListener("scroll",()=>{
-    if(window.pageYOffset > 100){
-        upButton.classList.add("active")
-    }else{
-        upButton.classList.remove("active")
-    }
-})
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    upButton.classList.add("active");
+  } else {
+    upButton.classList.remove("active");
+  }
+});
